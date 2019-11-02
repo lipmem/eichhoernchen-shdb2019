aacc49a9b0fd04d7083408400841178b1b4a71cb
philipp <tengamd@gmail.com>
2019-11-02 18:11:13 +0100

    new 1km file

--- app.py
+++ app.py
@@ -3,23 +3,28 @@ import pickle
 import networkx as nx
 import pandas as pd
 
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 
 ##tree_path = pickle.load(open("file.p", "rb"))
-def getPath(start_id,end_id):
-    p= nx.algorithms.shortest_paths.weighted.dijkstra_path(G,start_id,end_id)
-    return [(i,y[trees[trees.id==i].index][0],x[trees[trees.id==i].index][0]) for i in p]
+def getPath(start_id, end_id):
+    p = nx.algorithms.shortest_paths.weighted.dijkstra_path(
+        G, start_id, end_id)
+    return [(i, x[trees[trees.id == i].index][0], y[trees[trees.id == i].index][0]) for i in p]
+
 
 @app.route('/app/api/v1.0/trees/', methods=['GET'])
 def get_tasks():
     start_tree = request.args.get('start_tree', None)
     end_tree = request.args.get('end_tree', None)
-    trees=getPath(int(start_tree),int(end_tree))
+    trees = getPath(int(start_tree), int(end_tree))
 
     return jsonify({'trees': trees})
 
 
 if __name__ == '__main__':
-    [G,trees,x,y]=pickle.load(open( "routing_data.pickle", "rb" ))
+    [G, trees, x, y] = pickle.load(open("routing_data.pickle", "rb"))
     app.run()
--- scripts/scrape/index.js
+++ scripts/scrape/index.js
@@ -1,22 +1,51 @@
 const axios = require("axios");
 const fs = require("fs");
 
-for (let i = 1; i < 641; i++) {
-  axios
-    .get(
-      "https://trees.codefor.de/api/v2/trees/?dist=2000&page=" +
-        i +
-        "&point=13.381018%2C52.498606"
-    )
-    .then(function(response) {
-      // handle success
-      let data = JSON.stringify(response.data.features)
-        .substr(1)
-        .slice(0, -1);
-      fs.appendFile("data", data + ",", function(err) {
-        if (err) throw err;
-        console.log("Saved!");
-      });
-    })
-    .catch(err => console.log(err));
+function scrape() {
+  for (let i = 1; i < 327; i++) {
+    axios
+      .get(
+        "https://trees.codefor.de/api/v2/trees/?dist=1000&page=" +
+          i +
+          "&point=13.428948,52.499029"
+      )
+      .then(function(response) {
+        // handle success
+        let data = JSON.stringify(response.data.features)
+          .substr(1)
+          .slice(0, -1);
+        fs.appendFile("1km", data + ",", function(err) {
+          if (err) throw err;
+          console.log("Saved!");
+        });
+      })
+      .catch(err => console.log(err));
+  }
 }
+
+scrape();
+
+// for (let i = 1; i < 641; i++) {
+//   axios
+//     .get(
+//       "https://trees.codefor.de/api/v2/trees/?dist=2000&page=" +
+//         i +
+//         "&point=13.381018%2C52.498606"
+//     )
+//     .then(function(response) {
+//       for (let feat of response.data.features) {
+//         let tree = { [feat.id]: feat.geometry.coordinates };
+//         fs.appendFile(
+//           "trees.json",
+//           JSON.stringify(tree)
+//             .substr(1)
+//             .slice(0, -1) + ",\n",
+//           function(err) {
+//             if (err) throw err;
+//             console.log("Saved!");
+//           }
+//         );
+//       }
+//     })
+//     .catch(err => console.log(err));
+// }
